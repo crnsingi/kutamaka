36636ec50f9e4e8d4500fd86f87cf671756f2d64
Cesar <cesarnsingi@hotmail.com>
2024-12-29 01:36:34 +0100

    li Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,10 @@ const Navbar: React.FC = () => {
     return (
         <nav>
             <h1>Marketplace</h1>
+            <ul>
+                <li><Link></Link></li>
+                <li><Link></Link></li>
+            </ul>
         </nav>
 
     )
