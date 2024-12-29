18b03d8b5b28844c728130121c76d9cd89498731
Cesar <cesarnsingi@hotmail.com>
2024-12-29 06:51:56 +0100

    return router

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Router } from 'react-router-dom'
 
 const App: React.FC = () => { 
-  
+  return ( 
+    <Router>
+      
+    </Router>
+  )
+
 }
 export default App
