e5ddc7400d3e45cb9e8d22fb1d61c18818c71688
Cesar <cesarnsingi@hotmail.com>
2024-12-29 06:52:25 +0100

    return Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Router } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App: React.FC = () => { 
   return ( 
     <Router>
-      
+      <Navbar/>
+
     </Router>
   )
 
