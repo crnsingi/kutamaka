62149352f30915b33e600f79d3b9c1dd2d2d7e8a
Cesar <cesarnsingi@hotmail.com>
2024-12-29 05:20:17 +0100

    return p product description

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,6 +9,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     return (
         <div>
             <h2>{product.name}</h2>
+            <p>{product.description}</p>
         </div>
 
     )
