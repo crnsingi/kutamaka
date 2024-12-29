773dae494776893fd35291ec6f2560f486a8b644
Cesar <cesarnsingi@hotmail.com>
2024-12-29 06:26:51 +0100

    const products and set products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,10 @@ import ProductList from '../components/ProductList';
 import { Product } from '../types';
 
 const Home: React.FC = () => {
+    const [products, setProducts] = useState<Product[]>([
+        
+    ])
 
 }
 
+export default Home;
