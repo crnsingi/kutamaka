2ec083292716e57c435738f23c1a234f15dc1a1e
Cesar <cesarnsingi@hotmail.com>
2025-01-01 11:46:24 +0100

    export default function

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React from "react";
 function MyButton({ title }: { title: string }) {
     return (
         <button>{title}</button>
-    )
+    );
+}
 
+export default function MyApp() {
+    
 }
