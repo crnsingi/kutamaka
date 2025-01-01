aea4d3a984a999b064f02b5536ecfaf6e275fa98
Cesar <cesarnsingi@hotmail.com>
2025-01-01 10:47:03 +0100

    document add event listener

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,3 @@
-// src/index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+document.addEventListener("DOMContentLoaded", function() {
+    sayGreeting();
+});
